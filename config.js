d724622e65cac52e16a3e06c56cc14a749476536
unconfirmable <147227345+unconfirmable@users.noreply.github.com>
2023-10-06 22:19:25 -0700

    t

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://hello-world-misty-shape-f533.adamacqua.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZDIxZTBkNWE1N2Y5NTg3YmJmMzc3Y2Y4MmU5OGRiMSIsInN1YiI6IjY1MjBkYjNlYzUwYWQyMDBjOTFmNGY1MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jSYSw4oJVBNFMHb-ZK06PW4vDB8YWYflZUwlBFuj7-Q"
+};
